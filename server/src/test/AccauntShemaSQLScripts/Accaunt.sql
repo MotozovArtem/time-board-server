3df909a69a8a8c261be3b5c043e086c2ac98413a
tianid <tianid73@gmail.com>
2020-04-15 23:26:20 +0300

    TB-19 Added data init scripts for db

---
+++ server/src/test/AccauntShemaSQLScripts/Accaunt.sql
@@ -0,0 +1,75 @@
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('6f2228c5-76ff-44c4-b4ca-304994e90efa','sergey','123qwe321','MiaAld@mail.com','MIA','Aldridge','2020-04-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('b92f1bd4-d7c9-4cbd-8d62-1f4f5e9bc908','root','1234567','JamArc@mail.com','JAMES','Archibald','2020-03-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('db74f4f1-dadc-4b1f-a817-0aaafededfcc','irina','1234678','EmiAnd@mail.com','EMILY','Andrews','2020-04-11');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('5ffe3bd6-3940-4e44-89fe-dc014d46cb6c','user3','112233qwe','AvaAtt@mail.com','AVA','Attwood','2020-02-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('c209f968-617b-4781-ab8a-317ff8d72821','system','123qweasd','PopAtc@mail.com','POPPY','Atcheson','2020-04-05');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('7a9d07f2-9d6b-415e-aee4-7809dfd0f442','test','123qweasd123','FreAdr@mail.com','FREYA','Adrian','2020-01-22');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('e853b5b7-6ab5-451e-a0b7-047ac6f93d9e','natasha','1234567890qw','MuhBab@mail.com','MUHAMMAD','Babcock','2019-04-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('4e7a3879-0140-4162-a7e2-abe7eb999453','office','123qwe','GeoAdd@mail.com','GEORGE','Adderiy','2018-04-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('2ae0096f-66f2-4dc9-93e8-73ad4c2ec045','oleg','qazaqazaq','HarArn@mail.com','HARRY','Arnold','2019-03-05');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('0ce6a971-d2c0-46ef-916d-9974e171bb50','support','1q2w3e','ChaAyr@mail.com','CHARLIE','Ayrton','2015-04-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('e9931071-aec4-42ab-ba04-3eba8dbd6185','director','123456789','HenAud@mail.com','HENRY','Audley','2019-11-27');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('99592dd5-fbad-46f9-ae30-654cf93df3fc','tester','12345','LilAdd@mail.com','LILY','Addington','2016-04-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('e00ae2d4-962a-4bc1-b8b6-f9e61b4b207a','service','12qwaszx','EllAll@mail.com','ELLA','Allford','2020-03-02');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('090d0d1d-7a93-4e98-a980-0e9f1d8ecd53','manager','5q5q5q','OscAbr@mail.com','OSCAR','Abramson','2020-01-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('150e02e0-31e3-45f5-bb69-2b43cd68ad57','user','1qazzaq1','EthAnd@mail.com','ETHAN','Anderson','2019-07-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('c0cd0ad3-1654-4b5d-bedd-431e50033ea5','Sklad','Qazwsxe5','AmeAls@mail.com','AMELIA','Alsopp','2019-09-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('ec5ac159-6e1b-48b1-a4d7-0e1e6f5f51e9','admin','123123z','ScaAus@mail.com','SCARLETT','Austin','2017-04-14');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('8fcf281f-716b-4629-b28b-db4b9b4c31fb','adm','123456','OliAda@mail.com','OLIVER','Adamson','2019-11-20');
+
+INSERT INTO accounts.account(
+	id, login, password, email, first_name, second_name, creation_date)
+	VALUES ('f1f3af49-ddad-4972-a77d-59aae7375948','user1','qwerasdf1234','LeoArt@mail.com','LEO','Arthurs','2020-04-01');
---
+++ server/src/test/ProjectShemaSQLScripts/Step.sql
@@ -0,0 +1,31 @@
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('55e797e4-1c18-48e5-b4e9-1573970c5449', 'In Test');
+
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('41f020ab-33a8-4ce6-94f9-84d5763c5eb5', 'In Market');
+
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('eeaace03-fa8e-4554-b765-1281e77b71fa', 'In Review');
+
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('508c786a-cb91-4f97-b91d-897c6a884f8b', 'In Planing');
+
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('19eab84d-e9a9-43e6-96a5-da98f7e9cb89', 'In Dreaming');
+
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('f096a5b5-e24c-4c79-a5d1-6a4b0a811c6f', 'To do');
+
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('90bfd25b-1451-4338-bcd1-f0e12c22f249', 'In progress');
+
+INSERT INTO project_test.step(
+	id, name)
+	VALUES ('c9165da4-f69c-4d76-92cd-2befd7626a9b', 'Done');
